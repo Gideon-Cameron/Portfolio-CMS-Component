ff56de1f6e3812258106118b2f301b8d85403f68
Gideon Cameron <gideonwork20@gmail.com>
2025-07-28 00:19:17 -0700

    Creating dummy data untill the firestore is set up

---
+++ src/data/defaultContent.ts
@@ -0,0 +1,8 @@
+export const heroContent = {
+    intro: "Hi, my name is",
+    name: "Gideon Cameron.",
+    subtitle: "I build things for the web.",
+    description: `I'm a frontend developer specializing in building (and occasionally designing) exceptional, responsive,
+    high-performance digital experiences. Currently, I’m focused on building accessible, well designed and user friendly projects.`,
+  };
+  
