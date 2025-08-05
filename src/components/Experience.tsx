13bbb81c0fb707f6f16fb4c3943738dcec52f136
Gideon Cameron <gideonwork20@gmail.com>
2025-08-04 18:31:46 -0700

    Small styling changes

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -61,7 +61,7 @@ const Experience = () => {
       >
         <h2 className="text-2xl font-bold text-[#007acc] dark:text-[#64ffda] font-mono whitespace-nowrap">
           <span className="mr-2 font-mono text-[#007acc] dark:text-[#64ffda]">02.</span>
-          Experience
+          Where I've Worked
         </h2>
         <div className="h-px ml-5 flex-1 max-w-[300px] bg-[#8892b0] relative -top-[5px]" />
       </motion.div>
